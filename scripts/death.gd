013ecfdda9f3ada14be17a7f356ee6592b0d51d1
a2g2j <90936538+a2g2j@users.noreply.github.com>
2025-08-01 00:55:42 -0500

    Basics
    added tile player and collision also broken death plane idk whats wrong pmo

---
+++ scripts/death.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+
+@onready var timer: Timer = $Timer
+
+
+func _on_body_entered(body):
+	
+	print("you suck balls")
+	timer.start()
+	
+	
+
+
+func _on_timer_timeout():
+	get_tree().reload_current_scene()
