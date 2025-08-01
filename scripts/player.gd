527e774a4f230c769f60d686ad054b6c63b185bb
a2g2j <90936538+a2g2j@users.noreply.github.com>
2025-08-01 16:32:17 -0500

    Added asset tiles and physics + basic ass test level

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -800.0
+const SPEED = 200.0
+const JUMP_VELOCITY = -300.0
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
