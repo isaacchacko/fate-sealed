45496fb1ec0668deadbd9fefae6d642bfa0708e7
IshaanBansal2006 <ishaanbansal2006@gmail.com>
2025-08-01 18:25:32 -0500

    make exit instead of cred for now

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -5,8 +5,8 @@ func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
 
-func _on_credits_pressed() -> void:
-	pass # Replace with function body.
+func _on_exit_pressed() -> void:
+	get_tree().quit()
 
 
 func _on_how_to_pressed() -> void:
