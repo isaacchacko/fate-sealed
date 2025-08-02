9e65dd6784cd90fed88fbb5db9fb9c9ee8d95481
Isaac Chacko <isaac.chacko05@gmail.com>
2025-08-02 16:03:10 -0500

    fix door kill error

--- ender.gd
+++ ender.gd
@@ -15,4 +15,5 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 		game_won.emit()
 		# Optionally, wait for the animation to finish:
 		await ender.animation_finished
+		HistoryManager.reset_all()
 		get_tree().reload_current_scene()
