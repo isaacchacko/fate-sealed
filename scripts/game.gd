8bee9efd583188886189538b07bb94030fe0616d
a2g2j <90936538+a2g2j@users.noreply.github.com>
2025-08-02 17:53:30 -0500

    added player death code n shit also eagle death hitbox

--- scripts/game.gd
+++ scripts/game.gd
@@ -5,4 +5,17 @@ func _ready():
 	# allow for the death signal to trigger a stopwatch reset
 	var death = $death
 	var stopwatch = $stopwatch
+	var player = $player
+	var goonbadeath = get_node("goonba/goonbadeath")
+	var goonbadeath2 = get_node("goonba2/goonbadeath")
+	var eagle = get_node("eagle/goonbadeath")
+	death.player_died.connect(player.die)
 	death.player_died.connect(stopwatch.reset)
+	player.player_died.connect(player.die)
+	player.player_died.connect(stopwatch.reset)
+	goonbadeath.player_died.connect(player.die)
+	goonbadeath.player_died.connect(stopwatch.reset)
+	goonbadeath2.player_died.connect(player.die)
+	goonbadeath2.player_died.connect(stopwatch.reset)
+	eagle.player_died.connect(player.die)
+	eagle.player_died.connect(stopwatch.reset)
--- scripts/player.gd
+++ scripts/player.gd
@@ -9,19 +9,23 @@ const JUMP_VELOCITY = -300.0
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @export var properties := ["global_position"]
 
+var death_jump_velocity = -300  # Upward impulse (negative y is up)
+var gravity = 800
+var is_dead = false
+var death_velocity = Vector2.ZERO
+
 func _ready():
 	HistoryManager.register_node(self, properties, false)
 
-func check_deadly_collision():
-	var tilemap = get_node("../SpikeTileMap") # Adjust path as needed
-	var tile_coords = tilemap.local_to_map(global_position)
-	var tile_data = tilemap.get_cell_tile_data(0, tile_coords)  # 0 is the layer index
+func check_spike_collision():
+	if is_dead == false:
+		var tilemap = get_node("../SpikeTileMap") # Adjust path as needed
+		var tile_coords = tilemap.local_to_map(global_position)
+		var tile_data = tilemap.get_cell_tile_data(0, tile_coords)  # 0 is the layer index
 
-	if tile_data and tile_data.get_custom_data("deadly"):
-		print("you sssss balls")
-		player_died.emit()
-		HistoryManager.reset_all()
-		get_tree().reload_current_scene()
+		if tile_data and tile_data.get_custom_data("deadly"):
+			player_died.emit()
+
 		
 		
 
@@ -32,15 +36,20 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("fire"):
 		fire()
 
+	if is_dead:
+		velocity.y += gravity * delta  # Gravity pulls down
+
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	#movement is 1,0,-1
+	
 	var direction := Input.get_axis("move_left", "move_right")
+	if is_dead == true:
+		direction =0
 
-
-	check_deadly_collision()
+	check_spike_collision()
 		
 	#to flip sprite
 	if direction > 0:
@@ -58,15 +67,15 @@ func _physics_process(delta: float) -> void:
 
 
 	#choose which animations are played
-	if is_on_floor():
+	if is_on_floor() and is_dead == false:
 		if direction == 0:
 			animated_sprite_2d.play("IDLE")
 		elif !(direction == 0):
 			animated_sprite_2d.play("Running")
 	else:
-		if velocity.y < 0:
+		if velocity.y < 0 and is_dead == false:
 			animated_sprite_2d.play("jumping_up")
-		elif velocity.y > 0:
+		elif velocity.y > 0 and is_dead == false:
 			animated_sprite_2d.play("jumping_down")
 
 	if direction:
@@ -75,6 +84,23 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+func die():
+	print('player die')
+	is_dead = true
+	var deathY = global_position.y +162.5
+	print(global_position.y)
+	$Camera2D.limit_bottom = deathY
+	animated_sprite_2d.play("Death")
+	velocity.y = death_jump_velocity
+	collision_layer = 0
+	collision_mask = 0
+	var timer = Timer.new()
+	print("you sssss balls")
+	await get_tree().create_timer(1).timeout	
+	HistoryManager.reset_all()
+	get_tree().reload_current_scene()
+	
 
 func fire():
 	var bullet=bullet_path.instantiate()
