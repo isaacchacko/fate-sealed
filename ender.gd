5c155fb808c3d90e84173b1d27e1639fdbd8d7f9
Allen Thomas <aabmtho12@gmail.com>
2025-08-02 10:59:21 -0500

    added game ender

---
+++ ender.gd
@@ -0,0 +1,18 @@
+extends StaticBody2D
+
+@onready var ender: AnimatedSprite2D = $AnimatedSprite2D
+signal game_won
+var activated := false
+
+func _ready():
+	ender.play("idle")
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if not activated and not body.is_in_group("player"):  # Optionally check it's the player
+		activated = true
+		ender.play("donzo")
+		print("you the goat")
+		game_won.emit()
+		# Optionally, wait for the animation to finish:
+		await ender.animation_finished
+		get_tree().reload_current_scene()
