d41fdfea15b50a79eea4220e6f82009c3ee38ad1
a2g2j <90936538+a2g2j@users.noreply.github.com>
2025-08-02 01:26:59 -0500

    reverse stopwatch but broken will rewing infinite

--- scripts/stopwatch.gd
+++ scripts/stopwatch.gd
@@ -2,14 +2,23 @@ extends CanvasLayer
 
 var running := false
 var elapsed_time := 0.0
+var rewinding := false
 
 func _ready():
 	reset()
 	start()
 
 func _process(delta):
+	if Input.is_action_pressed("rewind"):
+		rewinding = true
+	else:
+		rewinding = false
+
 	if running:
-		elapsed_time += delta
+		if rewinding:
+			elapsed_time = max(0.0, elapsed_time - delta)
+		else:
+			elapsed_time += delta
 		$label.text = format_time(elapsed_time)
 
 func start():
