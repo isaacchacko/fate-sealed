372bd6b8d16c112060d4d1fdaee41cb6197a3285
Isaac Chacko <isaac.chacko05@gmail.com>
2025-08-01 23:24:01 -0500

    death works suck it boping

--- scripts/death.gd
+++ scripts/death.gd
@@ -1,16 +1,15 @@
-extends Area2D
-
-
-@onready var timer: Timer = $Timer
-
+extends StaticBody2D
 
 func _on_body_entered(body):
-
 	print("you suck balls")
-	timer.start()
 
 
 
+#func _on_timer_timeout():
+	#print("reload should occur")
+	#get_tree().reload_current_scene()
 
-func _on_timer_timeout():
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	print("you sssss balls")
 	get_tree().reload_current_scene()
