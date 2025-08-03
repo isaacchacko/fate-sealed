c10fffdeaca8cc5157ba4d7a054516f4edbe8be0
Allen Thomas <aabmtho12@gmail.com>
2025-08-03 09:53:42 -0500

    level2 is done take 3

---
+++ scripts/lvl_2.gd
@@ -0,0 +1,18 @@
+extends Node
+
+func _ready():
+	# Find all death sources and pass them to DeathManager
+	var death_sources = [
+		$death,
+		get_node("goonba/goonbadeath"),
+		get_node("goonba2/goonbadeath"),
+		get_node("eagle/goonbadeath"),
+		get_node("RedHulk/goonbadeath"),
+		get_node("DirtBlock/goonbadeath"),
+		get_node("player")  # Not sure if this is right; check if correct
+	]
+
+	var player = $player
+	var stopwatch = $stopwatch
+
+	DeathManager.setup(player, stopwatch, death_sources)
