3e8a236474667d291d1a11923506935761064daa
William Lam <williamlam5656@gmail.com>
2025-08-03 11:22:59 -0500

    last door takes to credtis scene

--- scripts/ender.gd
+++ scripts/ender.gd
@@ -17,5 +17,8 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 		var current_scene_file = get_tree().current_scene.scene_file_path
 		var next_level_number = current_scene_file.to_int() + 1
 		var next_level_path = "res://levels/lvl_" + str(next_level_number) + ".tscn"
-		get_tree().change_scene_to_file(next_level_path)
+		if FileAccess.file_exists(next_level_path):
+			get_tree().change_scene_to_file(next_level_path)
+		else:
+			get_tree().change_scene_to_file("res://scenes/credits.tscn")
 		HistoryManager.reset_all()
