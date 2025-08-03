155cd69e12015bc9e8a8c8c224cf0ecb71a88c65
a2g2j <90936538+a2g2j@users.noreply.github.com>
2025-08-03 02:51:13 -0500

    dirt block works

--- scripts/game.gd
+++ scripts/game.gd
@@ -9,6 +9,12 @@ func _ready():
 	var goonbadeath = get_node("goonba/goonbadeath")
 	var goonbadeath2 = get_node("goonba2/goonbadeath")
 	var eagle = get_node("eagle/goonbadeath")
+	var redhulk = get_node("RedHulk/goonbadeath")
+	var dirtblock = get_node("DirtBlock/goonbadeath")
+	var spike = get_node("player")
+
+
+
 	death.player_died.connect(player.die)
 	death.player_died.connect(stopwatch.reset)
 	player.player_died.connect(player.die)
@@ -19,3 +25,9 @@ func _ready():
 	goonbadeath2.player_died.connect(stopwatch.reset)
 	eagle.player_died.connect(player.die)
 	eagle.player_died.connect(stopwatch.reset)
+	redhulk.player_died.connect(player.die)
+	redhulk.player_died.connect(stopwatch.reset)
+	spike.player_died.connect(player.die)
+	spike.player_died.connect(stopwatch.reset)
+	dirtblock.player_died.connect(player.die)
+	dirtblock.player_died.connect(stopwatch.reset)
