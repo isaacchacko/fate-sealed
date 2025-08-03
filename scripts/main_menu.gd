a3059928757686061b125df901d7c37ee26a2cb1
William Lam <williamlam5656@gmail.com>
2025-08-03 08:19:22 -0500

    working level connection

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,8 +2,7 @@ extends Node2D
 
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/game.tscn")
-
+	get_tree().change_scene_to_file("res://levels/lvl_0.tscn")
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
